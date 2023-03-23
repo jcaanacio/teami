a2943289d10c4e502ff7e71bfe303be8345a0980
chrissie caintoy <chrissiecaintoy@gmail.com>
2023-03-23 18:50:16 +0800

    fix(models): changing entity name of sessions

--- src/main/java/com/bs280/teami/models/Session.java
+++ src/main/java/com/bs280/teami/models/Session.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity(name = "sessions")
+@Entity(name = "sessions1")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Session {
 
