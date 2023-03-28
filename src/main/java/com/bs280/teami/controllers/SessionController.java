509155b3efd523e9375aebf945c59f8dbd48aa80
Jay Christopher Anacio <30493303+jcaanacio@users.noreply.github.com>
2023-03-28 17:05:59 +0800

    feat(service): implementing service layer (#7)

--- src/main/java/com/bs280/teami/controllers/SessionController.java
+++ src/main/java/com/bs280/teami/controllers/SessionController.java
@@ -2,7 +2,6 @@ package com.bs280.teami.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,48 +14,40 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bs280.teami.models.Session;
-import com.bs280.teami.repositories.SessionRepository;
+import com.bs280.teami.services.SessionService;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
 
 public class SessionController {
     @Autowired
-    private SessionRepository sessionRepository;
+    private SessionService sessionService;
 
     @GetMapping
     public List<Session> list() {
-        return sessionRepository.findAll();
+        return sessionService.list();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Session create(@RequestBody final Session session) {
-        return sessionRepository.saveAndFlush(session);
+        return sessionService.create(session);
     }
 
     @GetMapping()
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id) {
-        return sessionRepository.findById(id).orElse(null);
+        return sessionService.get(id);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable Long id) {
-        /**
-         * TODO: Cascade delete
-         */
-        sessionRepository.deleteById(id);
+        sessionService.delete(id);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
     public Session update(@PathVariable Long id, @RequestBody Session session) {
-        /**
-         * TODO: Add validation that all attributes are passed in, otherwise return 400
-         *
-         * **/
-        Session existingSession = sessionRepository.getReferenceById(id);
-        BeanUtils.copyProperties(session, existingSession, "session_id");
-        return sessionRepository.saveAndFlush(existingSession);
+        return sessionService.update(id,session);
+
     }
 }
