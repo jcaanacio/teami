b1a470e58e043f63d8c87e7eb1d8b5860bd158f9
chrissie caintoy <chrissiecaintoy@gmail.com>
2023-03-30 14:05:50 +0800

    wip

--- src/main/java/com/bs280/teami/controllers/AttendeeController.java
+++ src/main/java/com/bs280/teami/controllers/AttendeeController.java
@@ -3,22 +3,52 @@ package com.bs280.teami.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.bs280.teami.models.Attendee;
-import com.bs280.teami.repositories.AttendeeRepository;
+import com.bs280.teami.services.AttendeeService;
 
 
 @RestController
 @RequestMapping("/api/v1/attendees")
 public class AttendeeController {
     @Autowired
-    private AttendeeRepository attendeesRepository;
+    private AttendeeService attendeesService;
 
     @GetMapping
     public List<Attendee> list(){
-        return attendeesRepository.findAll();
+        return attendeesService.list();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Attendee create(@RequestBody final Attendee attendee){
+        return attendeesService.create(attendee);
+    }
+
+    @GetMapping()
+    @RequestMapping("{id}")
+    public Attendee get(@PathVariable Long id) {
+        return AttendeeService.get(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        AttendeeService.delete(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Attendee update(@PathVariable Long id, @RequestBody Attendee attendee) {
+        return AttendeeService.update(id,attendee);
+
+    }
+
     
 }
---
+++ src/main/java/com/bs280/teami/services/AttendeeService.java
@@ -0,0 +1,39 @@
+package com.bs280.teami.services;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bs280.teami.models.Attendee;
+import com.bs280.teami.repositories.AttendeeRepository;
+
+@Service
+public class AttendeeService {
+    @Autowired
+    private static AttendeeRepository attendeeRepository;
+
+    public List<Attendee> list() {
+        return attendeeRepository.findAll();
+    }
+
+    public Attendee create(Attendee attendee) {
+        return attendeeRepository.saveAndFlush(attendee);
+    }
+
+    public static Attendee get(Long id) {
+        return attendeeRepository.findById(id).orElse(null);
+    }
+
+    public static void delete(Long id) {
+        attendeeRepository.deleteById(id);
+    }
+
+    public static Attendee update(Long id,Object attendee) {
+        Attendee existingAttendee = attendeeRepository.getReferenceById(id);
+        BeanUtils.copyProperties(attendee, existingAttendee, "attendee_id");
+        return attendeeRepository.saveAndFlush(existingAttendee);
+    }
+    
+}
